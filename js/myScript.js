fb690ae45889f32a26e0de795556d73f010fa122
boden1501 <htvinh1501@gmail.com>
2023-05-24 01:34:05 +0700

    no message

--- js/myScript.js
+++ js/myScript.js
@@ -21,36 +21,46 @@ $(document).ready(function () {
         showSlide(slideIndex);
     });
     function showSlide(n) {
-
-        // var slides = $(`#banner-hide img:nth(${slideIndex})`).attr('src');
-        // $("#mainImage").attr('src', slides);
-        // $("#mainImage").addClass("active"); // Add 'active' class to the image
-        // setTimeout(function () {
-        //     $("#mainImage").removeClass("active"); // Remove 'active' class after the transition
-        // }, 10);
-
-
         $("#mainImage").fadeOut(200, function () {
             var src = slide.eq(n).attr('src');
             $(this).attr('src', src).fadeIn(200);
         });
     }
 
+    LoadData();
+
+});
+
+function LoadData(){
 
     let s = ''
     $.each(dataProduct, function (k, v) {
         console.log(v)
-        s += ` <div class="product">
+        s += ` <div class="product" data-name="${v.name}" data-img="${v.img}" data-price="${v.price}">
                             <div class="img-product">
                                 <img src="${v.img}" alt="">
                             </div>
                             <div class="detail-product">
                                 <p class='name-product'>${v.name}</p>
                                 <p class='price'>${v.price}</p>
+                                
                             </div>
                 </div>`;
     })
+
     $('#top-product').html(s)
+    $('.product').click(function() {
 
-});
+        var name = $(this).data('name');
+        localStorage.setItem('name', name);
+        window.location = './html/product.html';
+        var price = $(this).data('price');
+        localStorage.setItem('price', price);
+        window.location = './html/product.html';
 
+        var img = $(this).data('img');
+        localStorage.setItem('img', img);
+        window.location = './html/product.html';
+      });
+      
+}
